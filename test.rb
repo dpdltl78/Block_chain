e7f0a9d57d2a3943df03993a2ad72b9987065657
ChorongPark <crpark@aim.co.kr>
2018-04-23 21:28:22 +0900

    test

---
+++ test.rb
@@ -0,0 +1,16 @@
+# encoding: UTF-8
+
+require 'sinatra'
+#시나트라 라는 걸 쓰겠다!!!! 이 훌륭한 도구를 ㅆ겠다!!
+
+#인스타가 이걸로 만들어졌다.  일단 대충 만들어보고 되나 안되나 확인용
+#현존하는 웹프레임워크에서 제일빠르다.
+#그다음은 파이썬 플라스크 >장고 /
+#우리는 루비 시나트라 >레일즈 
+get '/mine' do 
+	'you mine a block' 
+end
+get '/frank-says'do
+	'put this in your pipe & smoke it!'
+end
+
