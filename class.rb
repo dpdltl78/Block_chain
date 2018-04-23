2060bb408ac81dd165b4ffba6a17167b990b21ca
ChorongPark <crpark@aim.co.kr>
2018-04-23 22:33:02 +0900

    커밋연습

---
+++ class.rb
@@ -0,0 +1,9 @@
+class BlockChain #클래스 선언시 반드시 대문자 
+	#BlockChain 
+	#transaction
+	@attr_reader :chain, :transactions
+	#클래스 속성, : 속성이름1, 속성이름2
+#블록체인 생태계 - 블록들이 연결된 리스트 + 새로운 블록이 생길때 넣을 트랜잭션 정보들
+end
+#학생 클래스
+#블럭 클래스 
