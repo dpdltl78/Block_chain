2314767cf258936bc833148de4b250e31c7776a1
ChorongPark <crpark@aim.co.kr>
2018-04-26 21:47:04 +0900

    0426

--- block.rb
+++ block.rb
@@ -1,12 +1,14 @@
 # encoding: UTF-8
 
 require 'sinatra'
+require './Blockchain'
 #시나트라 라는 걸 쓰겠다!!!! 이 훌륭한 도구를 ㅆ겠다!!
 
 #인스타가 이걸로 만들어졌다.  일단 대충 만들어보고 되나 안되나 확인용
 #현존하는 웹프레임워크에서 제일빠르다.
 #그다음은 파이썬 플라스크 >장고 /
 #우리는 루비 시나트라 >레일즈 
+a = Blockchain.new
 get '/mine' do 
 	'you mine a block' 
 end
@@ -28,4 +30,12 @@ end
 
 get '/block_first_2'do
 	'8만원 '
+end
+
+get '/' do
+	a.mining.to_s
+end
+
+get '/mining' do
+	"지금 전체 블럭수는 :" +a.my_blocks.to_s
 end
